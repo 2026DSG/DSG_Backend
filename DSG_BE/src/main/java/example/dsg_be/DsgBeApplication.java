8d9b445016da829e014b7e698513d84038cbd93e
iokshoi <iokshoi0915@dsm.hs.kr>
2026-01-15 19:40:01 +0900

    feat
    프로젝트 생성

---
+++ DSG_BE/src/main/java/example/dsg_be/DsgBeApplication.java
@@ -0,0 +1,13 @@
+package example.dsg_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsgBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DsgBeApplication.class, args);
+    }
+
+}
---
+++ DSG_BE/src/test/java/example/dsg_be/DsgBeApplicationTests.java
@@ -0,0 +1,13 @@
+package example.dsg_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsgBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
