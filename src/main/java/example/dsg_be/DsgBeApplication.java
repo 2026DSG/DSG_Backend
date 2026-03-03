be1a134cd10970b5a5022d58b82e502e8501e2b2
iokshoi <iokshoi0915@dsm.hs.kr>
2026-03-03 12:16:32 +0900

    config
    env파일 추가

--- src/main/java/example/dsg_be/DsgBeApplication.java
+++ src/main/java/example/dsg_be/DsgBeApplication.java
@@ -1,5 +1,6 @@
 package example.dsg_be;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
@@ -9,6 +10,9 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 public class DsgBeApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
         SpringApplication.run(DsgBeApplication.class, args);
     }
 
