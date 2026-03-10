21d17facae734b6dcf6e3e4d7216548132a74091
Kim Minseo <u01056305988@gmail.com>
2026-03-10 22:02:08 +0900

    feat: MealType 검증로직 및 SecurityConfig에 apply 부분 추가
    - SecurityConfig에 apply 부분 추가
    - ErrorCode 추가
    - InvalidMealTypeException 추가
    - Enum에 검증 로직 추가

--- src/main/java/example/dsg_be/domain/apply/domain/MealType.java
+++ src/main/java/example/dsg_be/domain/apply/domain/MealType.java
@@ -1,8 +1,21 @@
 package example.dsg_be.domain.apply.domain;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import example.dsg_be.domain.apply.exception.InvalidMealTypeException;
+
 public enum MealType {
     LUNCH,
     LUNCH_SELF,   // 개인 부담
     DINNER,       // 학교 부담
-    DINNER_SELF   // 개인 부담
+    DINNER_SELF;   // 개인 부담
+
+    @JsonCreator
+    public static MealType from(String value) {
+        for (MealType mealType : MealType.values()) {
+            if (mealType.name().equalsIgnoreCase(value)) {
+                return mealType;
+            }
+        }
+        throw InvalidMealTypeException.EXCEPTION;
+    }
 }
