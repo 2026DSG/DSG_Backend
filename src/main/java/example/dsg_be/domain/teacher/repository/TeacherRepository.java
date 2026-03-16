4c2198298257edcd8745778c3c336c02626995ea
iokshoi <iokshoi0915@dsm.hs.kr>
2026-03-16 18:50:29 +0900

    refactor
    개행 삭제 및 사용하지 않는 메서드 제거

--- src/main/java/example/dsg_be/domain/teacher/repository/TeacherRepository.java
+++ src/main/java/example/dsg_be/domain/teacher/repository/TeacherRepository.java
@@ -3,7 +3,6 @@ package example.dsg_be.domain.teacher.repository;
 import example.dsg_be.domain.teacher.domain.TeacherEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-
 import java.util.List;
 import java.util.Optional;
 
@@ -13,6 +12,5 @@ public interface TeacherRepository extends JpaRepository<TeacherEntity, Long> {
     @Query("SELECT MAX(t.number) FROM TeacherEntity t WHERE t.isRemoved = false")
     Optional<Long> findMaxNumberByIsRemovedFalse();
 
-    boolean existsByName(String name);
     Optional<TeacherEntity> findByName(String name);
 }
